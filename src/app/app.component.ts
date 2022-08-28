385a308009b1a41eb36cc140f3d793d114f847e2
Your Name <you@example.com>
2022-08-28 14:00:36 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import {AppService} from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  sensorData : Object= [];
+  destroy$ : Subject<boolean> = new Subject<boolean>();
+
+
+  constructor(private appService: AppService) { }
+
+  ngOnInit() {
+
+    this.appService.getSensorData().pipe(takeUntil(this.destroy$)).subscribe((value:Object) => {
+      this.sensorData = value;
+    });
+  }
+
   title = 'sensor-readig-app';
 }
